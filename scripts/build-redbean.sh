67643aa821ebc898bf809e524e3ae8c1c9669f70
sebastian <sebastian@desktop>
2022-05-02 22:21:05 +0200

    chore: move hardcoded commands to scripts

---
+++ scripts/build-redbean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+export COSMO_GIT_REF=$(cat redbean-version.txt)
+git clone https://github.com/jart/cosmopolitan.git
+cd cosmopolitan
+git checkout $COSMO_GIT_REF
+sudo sh -c "echo ':APE:M::MZqFpD::/bin/sh:' >/proc/sys/fs/binfmt_misc/register"
+make -j8 o//tool/net/redbean.com >> build.log 2>&1
+mv build.log ..
+mv o/tool/net ../binaries
